3c8b71348fe674c10be2e69776bcf8567705f29f
Szu <a@a.hu>
2023-01-07 16:54:52 +0000

    first commit

---
+++ add.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import request
+
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '1111'
+
+
+@app.route('/', methods = ['GET','POST'])
+@app.route('/multiple', methods = ['GET','POST'])
+
+def multiple():
+    num1 = request.args.get('n1')
+    num2 = request.args.get('n2')
+    result = int(num1) * int(num2)
+    print(result)
+    return str(result)
+
+@app.route('/divide', methods = ['GET','POST'])
+
+def divide():
+    num1 = request.args.get('n1')
+    num2 = request.args.get('n2')
+    result = int(num1) / int(num2)
+    print(result)
+    return str(result)
+
+
+if __name__ == '__main__':
+   app.run(debug=True, host='0.0.0.0', port=80)
